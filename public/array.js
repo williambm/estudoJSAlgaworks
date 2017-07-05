c60c3db8aedd9a957f0e7c7e4cae1d4c1b1ad542
William <William@jarvis>
2017-07-05 13:21:15 -0300

    Exemplo básico de Array

---
+++ public/array.js
@@ -0,0 +1,31 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+/*Criando Arrya*/
+var celulares=[];
+
+/*Adicionando elementos no Array*/
+celulares.push('j5');
+celulares.push('MotoE');
+
+/*Listando Array*/
+console.log(celulares);
+
+/*colocando elementos no inicio do Array*/
+celulares.unshift('Iphone5');
+
+/*Listando Array*/
+console.log(celulares);
+
+/*Retirando do fim do Array*/
+console.log('Item retirado do final do Array: ',celulares.pop());
+
+/*Listando Array*/
+console.log(celulares);
+
+/*Retirando do Início do Array*/
+console.log('Item retirado do Início do Array: ',celulares.shift());
+
+/*Listando Array*/
+console.log(celulares);
