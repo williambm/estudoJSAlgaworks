b835ec0c78f5d6d90ec92be939dec2bf246f654c
William <William@jarvis>
2017-07-05 17:39:54 -0300

    Implementada a Lógica para calculo de taxas.

--- public/total.js
+++ public/total.js
@@ -2,3 +2,26 @@
  * http://usejsdoc.org/
  */
 console.log('Carregando...');
+
+/*Captura o valor do campos valor e total do formulário*/
+
+var valorElement = document.getElementById('valor');
+var totalElement = document.getElementById('total');
+
+console.log('valorElement',valorElement);
+
+valorElement.addEventListener('keyup', function(e) {
+	var valor = parseFloat(valorElement.value);
+	var total = 0;
+	
+	/*implementação de uma lógica de atribuição para total*/
+		if(valor<10){
+			total= valor +valor * 0.3 + 5;
+		}else if (valor>=10 && valor <50) {
+			total= valor+valor *0.1+2;
+		}else{
+			total=valor;
+		}
+	
+	totalElement.innerHTML = total;
+});
