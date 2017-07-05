cc3cf2da88db8890ab1de184fae2eac62e1b0c7c
William <William@jarvis>
2017-07-05 17:49:53 -0300

    Aperfeicoamento da logica de taxas.

--- public/total.js
+++ public/total.js
@@ -3,25 +3,25 @@
  */
 console.log('Carregando...');
 
-/*Captura o valor do campos valor e total do formulário*/
+/* Captura o valor do campos valor e total do formulário */
 
 var valorElement = document.getElementById('valor');
 var totalElement = document.getElementById('total');
 
-console.log('valorElement',valorElement);
+console.log('valorElement', valorElement);
 
 valorElement.addEventListener('keyup', function(e) {
-	var valor = parseFloat(valorElement.value);
+	var valor = parseFloat(valorElement.value ? valorElement.value : '0');
 	var total = 0;
-	
-	/*implementação de uma lógica de atribuição para total*/
-		if(valor<10){
-			total= valor +valor * 0.3 + 5;
-		}else if (valor>=10 && valor <50) {
-			total= valor+valor *0.1+2;
-		}else{
-			total=valor;
-		}
-	
+
+	/* implementação de uma lógica de atribuição para total */
+	if (valor > 0 && valor < 10) {
+		total = valor + valor * 0.3 + 5;
+	} else if (valor >= 10 && valor < 50) {
+		total = valor + valor * 0.1 + 2;
+	} else {
+		total = valor;
+	}
+
 	totalElement.innerHTML = total;
 });
