b0c6451860e2591a5dce6749a6b3f4aebf1876d8
William <William@jarvis>
2017-07-06 09:03:14 -0300

    Criação de um exercicio de seletores com Jquery

---
+++ public/seletores.js
@@ -0,0 +1,7 @@
+/**
+ * http://usejsdoc.org/
+ */
+$(Function(){
+	var selecaoTodosUsuarios = $('#selecao-todos-usuarios');
+	console.log('Seleção de todos os usuários: ',selecaoTodosUsuarios);
+});
