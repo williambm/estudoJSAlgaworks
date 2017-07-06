d448209c8462b8f76c8f2abc1510401c08c32945
William <William@192.168.15.21>
2017-07-06 19:30:12 -0300

    Seletores manipulando CSS

--- public/jquerySeletores/manipulando-css.js
+++ public/jquerySeletores/manipulando-css.js
@@ -2,6 +2,11 @@ $(function() {
 	var selecaoTodosUsuarios = $('#selecao-todos-usuarios');
 	var selecaoUsuarios = $('.js-selecao-usuario');
 
+	function estilizarLinhaUsuario(){
+		selecaoUsuarios.filter(':checked').parents('tr').addClass('Selecionado');
+		selecaoUsuarios.filter(':not(:checked)').parents('tr').removeClass('Selecionado');
+	}
+	
 	selecaoUsuarios.on('click', function() {
 		var totalUsuariosSelecionados = selecaoUsuarios.filter(':checked').length;
 		var checked = selecaoUsuarios.length === totalUsuariosSelecionados;
@@ -10,6 +15,23 @@ $(function() {
 
 	selecaoTodosUsuarios.on('click', function() {
 		selecaoUsuarios.prop('checked', selecaoTodosUsuarios.prop('checked'));
+		/*if(selecaoTodosUsuarios.prop('checked')){
+			selecaoUsuarios.parents('tr').addClass('Selecionado');
+		}else{
+			selecaoUsuarios.parents('tr').removeClass('Selecionado');
+		}*/
+		estilizarLinhaUsuario();
 	});
 
+	selecaoUsuarios.on('change', function(evento){
+		var selecaoUsuario =$(evento.target);
+		/*if(selecaoUsuario.prop('checked')){
+			selecaoUsuario.parents('tr').addClass('Selecionado');
+		}else {
+			selecaoUsuario.parents('tr').removeClass('Selecionado');
+		}*/
+		
+		estilizarLinhaUsuario();		
+		console.log(selecaoUsuario);
+	});
 });
