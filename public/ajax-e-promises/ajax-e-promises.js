e3e9a04e5522800cf07bf9c78ddc41d7de611570
William <William@jarvis>
2017-07-13 15:06:58 -0300

    manipulando o DOM com jquery e o callback do ajax.

--- public/ajax-e-promises/ajax-e-promises.js
+++ public/ajax-e-promises/ajax-e-promises.js
@@ -1,21 +1,30 @@
 /**
  * http://usejsdoc.org/
  */
-var resposta = $(function(){
-	$.ajax({
+$(function(){
+	var resposta = $.ajax({
 		url:'http://localhost:8080/estados',
 		method:'GET',
 		dataType:'jsonp',
 	});
 
+	resposta.done(function(estados){
+		var comboEstado = $('#combo-estado');
+		estados.forEach(function(estado){
+			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
+			console.log(optionEstado);				
+		});
+	});
+	
+	resposta.done(function(estados){
+		console.log('Funcionou mesmo !!!');
+	});
+
+	resposta.fail(function(){
+		alert('Erro carregando estados do servidor!');
+	});
 });
 
-resposta.done(function(){
-	console.log('tudo certo',estados);
-});
 
-resposta.done(function(){
-	console.log('Funcionou mesmo !!!');
-});
 
  
