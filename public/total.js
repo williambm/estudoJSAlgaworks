e6e3f3d06ba94eb43841b9e515f81b60529f0786
williambm <williambm@gmail.com>
2017-07-14 11:40:20 -0300

    Adicionado pluguin maskMoney para tratar os campos monetários.

--- public/total.js
+++ public/total.js
@@ -4,23 +4,35 @@
 
 /* Captura o valor do campos valor e total do formulário */
 
-var valorElement = document.getElementById('valor');
-var totalElement = document.getElementById('total');
+$(function() {
+    var valorElement = $('#valor');
+    var totalElement = document.getElementById('total');
+    var totalmascara = $('#total-mascara');
+    var mascaraConfig = {
+        decimal:',',
+        thousands:'.',
+        prefix:'R$ '        
+    };
 
-console.log('valorElement', valorElement);
+    console.log('valorElement', valorElement);
 
-valorElement.addEventListener('keyup', function(e) {
-	var valor = parseFloat(valorElement.value ? valorElement.value : '0');
-	var total = 0;
+    valorElement.on('keyup', function(e) {
+        var valor = valorElement.maskMoney('unmasked')[0];
+        //var valor = parseFloat(valorElement.val() ? valorElement.val() : '0');
+        var total = 0;
 
-	/* implementação de uma lógica de atribuição para total */
-	if (valor > 0 && valor < 10) {
-		total = valor + valor * 0.3 + 5;
-	} else if (valor >= 10 && valor < 50) {
-		total = valor + valor * 0.1 + 2;
-	} else {
-		total = valor;
-	}
-
-	totalElement.innerHTML = total;
+        /* implementação de uma lógica de atribuição para total */
+        if (valor > 0 && valor < 10) {
+            total = valor + valor * 0.3 + 5;
+        } else if (valor >= 10 && valor < 50) {
+            total = valor + valor * 0.1 + 2;
+        } else {
+            total = valor;
+        }
+        totalmascara.maskMoney(mascaraConfig);
+        totalmascara.maskMoney('mask',total);
+        totalElement.innerHTML = totalmascara.val();
+        
+    });
+    valorElement.maskMoney(mascaraConfig);
 });
