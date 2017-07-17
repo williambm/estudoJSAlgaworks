6858dedc0d63638da784d3d60fc379a11c14bf6a
williambm <williambm@gmail.com>
2017-07-17 12:02:52 -0300

    Evolução do exemplo próprio, formulario-dinamico.

--- public/questionario/formulario-wbm.js
+++ public/questionario/formulario-wbm.js
@@ -1,11 +1,17 @@
-$(function(){
-    $("select" ).change(function(){
+$(function() {
+    $("select").change(function () {
         
     var selecao = "";
-        $('select option:selected').each(function(){
+    var $divFeminino = "<div class='form-group'><label>Nome</label></div>";//qual a melhor forma de inserir conteúdo por html com jquery??
+        $('select option:selected').each(function() {
            selecao = $(this).text();
         });
         console.log(selecao);
+        console.log(typeof($divFeminino));
+        $("select").append($divFeminino);
+            if(selecao === "Feminino") {
+                $(".escolha-genero").html("<h2>Foi!</h2>");//a forma de usar o append está errada, por que ???Resp.1 -> estou apenas fazendo replace.... mas não estou inserindo um novo elemento este está sendo o problema!!
+        } 
     });
     
 });
