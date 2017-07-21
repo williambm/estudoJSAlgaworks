c20c144e2e9928795788707ede516a8209085c38
unknown <william@enf.usp>
2017-07-21 16:35:29 -0300

    exemplo de module-pattern.

--- public/module-pattern/module-pattern.js
+++ public/module-pattern/module-pattern.js
@@ -1,7 +1,16 @@
-var contador = function(){
+var contador = (function(){
     var valor = 0; //Como o valor está dentro da função ele fica como privado deste forma.
     
     return {
-        incrementar:
+        incrementar: function(){
+            valor++;
+        },
+        reset: function(){
+            console.log('valor do contador: ', valor);
+            valor = 0;
+        }
     }
-}
+}());
+contador.incrementar();
+contador.incrementar();
+contador.reset();
