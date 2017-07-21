ba785dfd2085555d8e676ddc61adbf2b329d2b1e
unknown <william@enf.usp>
2017-07-21 07:04:09 -0300

    Inserido mascara de data de nascimento ao formulário.

--- public/questionario/formulario-wbm.js
+++ public/questionario/formulario-wbm.js
@@ -1,20 +1,27 @@
 ﻿$(function() {
     $("select").change(function () {
-        
-    var selecao = "";
-    var $abreDivForm = "<div class='form-group form-atividade'>",
+    
+        //variaves que criam o HTML    
+        var selecao = "";
+        var $abreDivForm = "<div class='form-group form-atividade'>",
         nome = "<label>Nome</label><input class='form-control'></input>",
         pergunta = "<br><label>Quais destas atividades você mais gosta?</label>",
         atividadesFemininas = ("<div class='checkbox atividades'><label><input type='checkbox' value='1'>Correr no parque</label><br>" +
                               "<label><input type='checkbox' value='2'>Pedalar</label><br>" +
-                              "<label><input type='checkbox' value='3'>Jogar Volei</label><br>" ),
+                              "<label><input type='checkbox' value='3'>Jogar Volei</label><br>" +
+                              "<br><label>Data de nascimento</label><input type='text' class='form-control date' name='field-name'/>"),
         atividadesMasculinas = ("<div class='checkbox atividades'><label><input type='checkbox' value='1'>Tomar cerveja</label><br>" +
                               "<label><input type='checkbox' value='2'>Pedalar</label><br>" +
-                              "<label><input type='checkbox' value='3'>Jogar Videogame</label><br>" ),
+                              "<label><input type='checkbox' value='3'>Jogar Videogame</label><br>" +
+                              "<br><label>Data de nascimento</label><input type='text' class='form-control date' name='field-name'/>" ),
         fechaDiv = "</div></div>";
         
         
+        //Função que cuida das mascaras dos campos de datas e afins...
+        $(".date").mask('00/00/0000');        
         
+        
+        //Evento que captura o texto do combobox
         $('select option:selected').each(function() {
            selecao = $(this).text();
         });
