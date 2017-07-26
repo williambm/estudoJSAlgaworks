5fb8e03c07fe93edb26c5aa8ac4fdc2d1f5e4a7f
williambm <williambm@gmail.com>
2017-07-26 06:32:39 -0300

    no ajax-promises.js foi add module pattern e foi criado novo arquivo js para usar bind.

--- public/ajax-e-promises/ajax-e-promises.js
+++ public/ajax-e-promises/ajax-e-promises.js
@@ -1,33 +1,72 @@
 /**
  * http://usejsdoc.org/
  */
-$(function(){
-	var resposta = $.ajax({
+
+
+var estado = (function(){
+    var comboEstado = $('#combo-estado');
+    
+    function iniciar(){        
+        $.ajax({
 		url:'http://localhost:8080/estados',
 		method:'GET',
 		dataType:'jsonp',
-	});
-
-	resposta.done(function(estados){
-		var comboEstado = $('#combo-estado');
-		//comboEstado.empty();
-		comboEstado.html('<option>Selecione o estado</option>');
+        success: onEstadosRetornados,
+        error: onError
+        });
+    }
+    
+    function onEstadosRetornados(estados){
+        comboEstado.html('<option>Selecione o estado</option>');
 		estados.forEach(function(estado){
-			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
-			console.log(optionEstado);
-			comboEstado.append(optionEstado);
-										
+		var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
+		console.log(optionEstado);
+		comboEstado.append(optionEstado);
 		});
-	});
-	
-	resposta.done(function(estados){
-		console.log('Funcionou mesmo !!!');
-	});
+        
+    }
 
-	resposta.fail(function(){
-		alert('Erro carregando estados do servidor!');
-	});
-});
+    function onError(){
+        alert('Erro carregando estados do servidor!');
+    }
+    
+    return {
+        iniciar: iniciar
+    }
+})();
+
+estado.iniciar();
+
+
+
+
+
+//$(function(){
+//	var resposta = $.ajax({
+//		url:'http://localhost:8080/estados',
+//		method:'GET',
+//		dataType:'jsonp',
+//	});
+//
+//	resposta.done(function(estados){
+//		
+//		//comboEstado.empty();
+//		comboEstado.html('<option>Selecione o estado</option>');
+//		estados.forEach(function(estado){
+//			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
+//			console.log(optionEstado);
+//			comboEstado.append(optionEstado);//										
+//		});
+//	});
+//	
+//	resposta.done(function(estados){
+//		console.log('Funcionou mesmo !!!');
+//	});
+//
+//	resposta.fail(function(){
+//		alert('Erro carregando estados do servidor!');
+//	});
+//});
 
 
 
